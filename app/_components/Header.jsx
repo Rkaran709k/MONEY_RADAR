0fb3845a4006c0185a72563611693952d7fb2d1b
crispyhambun <shinigami709k@gmail.com>
2024-08-30 22:29:48 +0530

    shadow changes

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 function Header() {
   return (
-    <div className='p-5 flex justify-between items-center shadow-md'>
+    <div className='p-5 flex justify-between items-center shadow-sm'>
       <Image src={'./logo.svg'}
       alt = 'logo'
       width={150}
